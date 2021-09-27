c49ac7c8d1258854f118fd01f1dea30483201c19
Gastón Bertolani <gaston.bertolani@gmail.com>
2021-09-27 14:35:22 -0300

    [ADD]Purchase Requisition Module

---
+++ l10n_ar_purchase_requisition/__manifest__.py
@@ -0,0 +1,35 @@
+###############################################################################
+#
+#    Copyright (c) 2018 Eynes/E-MIPS (www.eynes.com.ar)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    "name": "l10n Ar Purchase Requisition",
+    "category": "L10N AR",
+    "version": "12.0.1.0.0",
+    "author": "Eynes/E-MIPS",
+    "license": "AGPL-3",
+    "description": "This module adapts the purchase requisition for Argentina.",
+    "depends": [
+        "purchase_requisition",
+    ],
+    "data": [
+        'views/purchase_requisition_view.xml',
+    ],
+    "installable": True,
+    "application": True,
+}
