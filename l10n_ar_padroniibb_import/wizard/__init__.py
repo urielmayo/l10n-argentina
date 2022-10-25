88538e7d8c0451d382fca5f5fcf6c6cd156ab6d0
Jose Gabriel Tortolero <gabriel.tortolero@eynes.com.ar>
2022-10-25 08:40:18 -0400

    padron mass update cordoba

--- l10n_ar_padroniibb_import/wizard/__init__.py
+++ l10n_ar_padroniibb_import/wizard/__init__.py
@@ -13,3 +13,4 @@ from . import padron_tucuman #  noqa
 from . import padron_mass_update_santa_fe # noqa
 from . import padron_mass_update_jujuy # noqa
 from . import padron_cordoba # noqa
+from . import padron_mass_update_cordoba # noqa
