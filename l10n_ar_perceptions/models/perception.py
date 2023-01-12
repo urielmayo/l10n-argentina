55539cd73944239d751e97a51bcb25dc9c9f7f78
JoaquinZanardi <joaquin.zanardi@eynes.com.ar>
2023-01-12 17:47:15 -0300

    [FIX] perception not applicable returns None and have to be checked before adding it to invoice preception lines

--- l10n_ar_perceptions/models/perception.py
+++ l10n_ar_perceptions/models/perception.py
@@ -403,7 +403,9 @@ class PerceptionPerception(models.Model):
             # TODO: compute no crea percepciones,
             # por ende  al llamar a _compute_perception_invoice_taxes
             # no genera ningun ait nuevo
-            perc_lines += perc.compute(partner_perc, **kwargs)
+            result = perc.compute(partner_perc, **kwargs)
+            if result is not None:
+                perc_lines += result
         return perc_lines
 
 
